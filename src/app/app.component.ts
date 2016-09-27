f761ac4e68ba8d32e52c8e8d5068102af3126ed8
Julien Ciais <julien.ciais@orange.com>
2016-09-27 15:51:57 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { AngularFire } from 'angularfire2';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+
+  constructor(af: AngularFire) {
+
+  }
+
+}

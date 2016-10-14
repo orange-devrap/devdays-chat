faead99f1b501377011af20d23b40c5a0f0d7b9f
Julien Ciais <julien.ciais@orange.com>
2016-10-14 15:01:48 +0200

    import never used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods } from 'angularfire2';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
 import { AuthService } from './shared/services';
 
 @Component({
